bba6a85e2c2f4f2fffcabbd87c4b75b46ee50873
Administrator <Administrator@SC-201907201119>
2020-03-20 22:07:27 +0800

    init

--- easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/ChatStarterApplication.java
+++ easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/ChatStarterApplication.java
@@ -1,4 +1,4 @@
-package org.easymis.easysaas.icc;
+package org.easymis.easyicc.web.chat;
 
 
 
@@ -16,17 +16,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan({"*.easymis.easysaas.*"})
+@ComponentScan({"*.easymis.easyicc.*"})
 @EnableTransactionManagement(order = 2) // 设置事务执行顺序(需要在切换数据源之后，否则只走主库)
-@MapperScan({"*.easymis.easysaas.**.mapper"})
+@MapperScan({"*.easymis.easyicc.**.mapper"})
 @ServletComponentScan
 @EnableSwagger2
-public class IccApplication {
-	protected static final Logger logger = LoggerFactory.getLogger(IccApplication.class);
+public class ChatStarterApplication {
+	protected static final Logger logger = LoggerFactory.getLogger(ChatStarterApplication.class);
 
 	public static void main(String[] args) {
 		logger.info("web开始加载");
-		SpringApplication.run(IccApplication.class, args);
+		SpringApplication.run(ChatStarterApplication.class, args);
 		logger.info("web加载完毕");
 	}
 
