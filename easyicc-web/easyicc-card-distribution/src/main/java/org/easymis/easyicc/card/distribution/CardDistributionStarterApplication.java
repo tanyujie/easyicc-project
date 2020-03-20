0d37555b0f06ee3c2cf9b96b370df7ac36876b1d
Administrator <Administrator@SC-201907201119>
2020-03-20 22:21:09 +0800

    init

--- easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/card/distribution/CardDistributionStarterApplication.java
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/card/distribution/CardDistributionStarterApplication.java
@@ -1,4 +1,4 @@
-package org.easymis.easysaas.icc;
+package org.easymis.easyicc.card.distribution;
 
 
 
@@ -21,12 +21,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan({"*.easymis.easysaas.**.mapper"})
 @ServletComponentScan
 @EnableSwagger2
-public class IccApplication {
-	protected static final Logger logger = LoggerFactory.getLogger(IccApplication.class);
+public class CardDistributionStarterApplication {
+	protected static final Logger logger = LoggerFactory.getLogger(CardDistributionStarterApplication.class);
 
 	public static void main(String[] args) {
 		logger.info("web开始加载");
-		SpringApplication.run(IccApplication.class, args);
+		SpringApplication.run(CardDistributionStarterApplication.class, args);
 		logger.info("web加载完毕");
 	}
 
