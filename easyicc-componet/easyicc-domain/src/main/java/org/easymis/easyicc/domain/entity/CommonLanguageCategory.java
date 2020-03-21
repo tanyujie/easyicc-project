4a7d3bbcc7c9b11aed8c8bad0dfbc056d7636127
Administrator <Administrator@10.0.0.1>
2020-03-21 23:00:47 +0800

    init

--- easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/CommonLanguageCategory.java
+++ easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/CommonLanguageCategory.java
@@ -1,5 +1,19 @@
 package org.easymis.easyicc.domain.entity;
 
+import lombok.Data;
+
+@Data
 public class CommonLanguageCategory {
 
+	private String id;
+
+	private String orgId;
+
+	private String name;
+
+	private String depict;
+
+	private String parentId;
+
+	private Integer priority;
 }
