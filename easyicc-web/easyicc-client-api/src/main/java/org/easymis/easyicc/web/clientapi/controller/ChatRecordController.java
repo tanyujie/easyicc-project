55eaf7a95b423edbdee90de6c1b8737cf63bbfea
Administrator <Administrator@10.0.0.1>
2020-03-21 23:28:01 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/ChatRecordController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/ChatRecordController.java
@@ -1,11 +1,19 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.ChatRecord;
+import org.easymis.easyicc.domain.entity.School;
 import org.easymis.easyicc.service.ChatRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @Api(value = "/chatRecord", description = "对话记录")
 @Controller
@@ -13,4 +21,70 @@ import io.swagger.annotations.Api;
 public class ChatRecordController extends IdentityRepository{
 	@Autowired
 	private ChatRecordService service;
+	@ApiOperation(value = "查询接口", response = School.class)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "content", value = "常用语内容", dataType = "string", required = false),
+			@ApiImplicitParam(name = "pageNum", value = "页码", dataType = "int", required = false),
+			@ApiImplicitParam(name = "pageSize", value = "每页显示记录", dataType = "int", required = false), })
+	@RequestMapping(value = { "/findPage.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult findByOrgId(String content, Integer pageNum, Integer pageSize) {
+		String orgId = getOrgId();
+		ChatRecord bean = new ChatRecord();
+/*		bean.setOrgId(orgId);
+		bean.setStaffId(getStaffId());
+		bean.setContent(content);*/
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		return RestResult.buildSuccess(service.find(bean, pageNum, pageSize));
+	}
+
+	@ApiOperation(value = "保存个人常用语")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "content", value = "常用语内容", dataType = "string", required = false),
+			@ApiImplicitParam(name = "type", value = "类型", dataType = "string", required = false),
+			@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+			@ApiImplicitParam(name = "title", value = "常用语标题", dataType = "string", required = false),
+			@ApiImplicitParam(name = "hotKey", value = "hotKey", dataType = "string", required = false), })
+	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult add(ChatRecord bean) {
+/*		bean.setOrgId(getOrgId());
+		bean.setStaffId(getStaffId());*/
+		if (service.save(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "修改个人常用语信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "content", value = "常用语内容", dataType = "string", required = false),
+			@ApiImplicitParam(name = "type", value = "类型", dataType = "string", required = false),
+			@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+			@ApiImplicitParam(name = "title", value = "常用语标题", dataType = "string", required = false),
+			@ApiImplicitParam(name = "hotKey", value = "hotKey", dataType = "string", required = false), })
+	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult update(ChatRecord bean) {
+		if (service.update(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "查看个人常用语信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "个人常用语id", dataType = "string", required = false), })
+	@RequestMapping(value = { "/read.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult read(String id) {
+		return RestResult.buildSuccess(service.findById(id));
+	}
+
+	@ApiOperation(value = "删除个人常用语信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "个人常用语id列表", dataType = "string", required = false), })
+	@RequestMapping(value = { "/delete.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult delete(String ids) {
+		return service.deleteByIds(ids);
+	}
 }
