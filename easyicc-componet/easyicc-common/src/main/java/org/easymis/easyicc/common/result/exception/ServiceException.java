a8f570c34b2a79e5925ed20db80c59a499d2a152
Administrator <Administrator@10.0.0.1>
2020-03-22 12:54:54 +0800

    init

---
+++ easyicc-componet/easyicc-common/src/main/java/org/easymis/easyicc/common/result/exception/ServiceException.java
@@ -0,0 +1,11 @@
+package org.easymis.easyicc.common.result.exception;
+
+/**
+ * Created by zhoutaoo on 2018/6/2.
+ */
+public class ServiceException extends BaseException {
+
+    //TODO 对业务异常的返回码进行校验，规范到一定范围内
+
+
+}
