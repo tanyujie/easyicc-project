7a347b4058b75eda9287d24c8dfa77fa56946da8
tanyujie <13551259347@139.com>
2020-03-23 17:21:46 +0800

    init

---
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/RebotQuestionCategoryController.java
@@ -0,0 +1,13 @@
+package org.easymis.easyicc.web.clientapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/rewriteRule", description = "知识库分类")
+@Controller
+@RequestMapping("/rebotQuestionCategory")
+public class RebotQuestionCategoryController {
+
+}
---
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/RebotQuestionController.java
@@ -0,0 +1,13 @@
+package org.easymis.easyicc.web.clientapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/rewriteRule", description = "知识库配置")
+@Controller
+@RequestMapping("/rebotQuestion")
+public class RebotQuestionController {
+
+}
