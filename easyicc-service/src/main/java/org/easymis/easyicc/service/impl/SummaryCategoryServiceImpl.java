2cb98b5b35983437ce42d40f084b5e4d399dad40
tanyujie <13551259347@139.com>
2020-03-23 14:19:19 +0800

    init

---
+++ easyicc-service/src/main/java/org/easymis/easyicc/service/impl/SummaryCategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package org.easymis.easyicc.service.impl;
+
+import org.easymis.easyicc.service.SummaryCategoryService;
+import org.springframework.stereotype.Service;
+@Service
+public class SummaryCategoryServiceImpl implements SummaryCategoryService{
+
+}
