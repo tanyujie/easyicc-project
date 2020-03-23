4a6a4ad7b502f143f407d0a28459df598d6c48a4
tanyujie <13551259347@139.com>
2020-03-23 17:09:45 +0800

    init

---
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/WechatSettingController.java
@@ -0,0 +1,13 @@
+package org.easymis.easyicc.web.clientapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/wechatSetting", description = "微信同步管理")
+@Controller
+@RequestMapping("/wechatSetting")
+public class WechatSettingController {
+
+}
