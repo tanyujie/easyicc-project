b77de86b265317730183c99253b4c3490caf28cb
tanyujie <13551259347@139.com>
2020-03-25 13:48:07 +0800

    init

---
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/controller/DistributionCardController.java
@@ -0,0 +1,13 @@
+package org.easymis.easyicc.web.card.distribution.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/distributionCard", description = "名片分配")
+@Controller
+@RequestMapping("/distributionCard")
+public class DistributionCardController {
+
+}
