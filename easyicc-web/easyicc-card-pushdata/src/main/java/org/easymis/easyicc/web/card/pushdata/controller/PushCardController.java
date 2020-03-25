ee985baaf473deaa47bbc12180830815c249a4b1
tanyujie <13551259347@139.com>
2020-03-25 13:48:29 +0800

    init

---
+++ easyicc-web/easyicc-card-pushdata/src/main/java/org/easymis/easyicc/web/card/pushdata/controller/PushCardController.java
@@ -0,0 +1,13 @@
+package org.easymis.easyicc.web.card.pushdata.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/pushCard", description = "名片推送")
+@Controller
+@RequestMapping("/pushCard")
+public class PushCardController {
+
+}
