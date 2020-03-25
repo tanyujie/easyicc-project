61266cff7a1e793e82cc6de8c756148470d67427
tanyujie <13551259347@139.com>
2020-03-25 12:32:50 +0800

    init

--- easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/service/DistributionCardService.java
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/service/DistributionCardService.java
@@ -1,5 +1,15 @@
 package org.easymis.easyicc.web.card.distribution.service;
 
-public interface DistributionCardService {
+import java.util.List;
 
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.Card;
+
+public interface DistributionCardService {
+    /**
+     * 分配名片
+     * @param cardList
+     * @throws Exception
+     */
+    RestResult allocationCard(List<Card> cardList, String serveName);
 }
--- easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/service/impl/DistributionCardServiceImpl.java
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/service/impl/DistributionCardServiceImpl.java
@@ -1,7 +1,18 @@
 package org.easymis.easyicc.web.card.distribution.service.impl;
 
+import java.util.List;
+
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.Card;
 import org.easymis.easyicc.web.card.distribution.service.DistributionCardService;
 
 public class DistributionCardServiceImpl implements DistributionCardService {
-
+    /**
+     * 分配名片
+     * @param cardList
+     * @throws Exception
+     */
+    public RestResult allocationCard(List<Card> cardList, String serveName) {
+    	return null;
+    }
 }
