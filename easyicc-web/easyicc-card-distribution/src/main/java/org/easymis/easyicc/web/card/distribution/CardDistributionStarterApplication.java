a8ceacc0210b1084a3df16c13c6a7a9f9f31eb41
Administrator <Administrator@10.0.0.1>
2020-03-26 23:40:28 +0800

    initial

--- easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/CardDistributionStarterApplication.java
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/CardDistributionStarterApplication.java
@@ -10,12 +10,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 @ComponentScan({"*.easymis.easyicc.*"})
 @EnableTransactionManagement(order = 2) // 设置事务执行顺序(需要在切换数据源之后，否则只走主库)
 @MapperScan({"*.easymis.easyicc.**.mapper"})
