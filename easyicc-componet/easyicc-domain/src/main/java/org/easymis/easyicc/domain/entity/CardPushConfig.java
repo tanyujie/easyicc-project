baf4546369e8aaf2a8764ab16953a6f3313ed52c
tanyujie <13551259347@139.com>
2020-03-27 19:02:41 +0800

    init

--- easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/CardPushConfig.java
+++ easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/CardPushConfig.java
@@ -1,5 +1,23 @@
 package org.easymis.easyicc.domain.entity;
 
+import lombok.Data;
+
+@Data
 public class CardPushConfig {
+	private String id;
+	private String orgId;
+	private String config;
+	private String tpl;
+	private String PARAM_TYPE;
+	private Integer status;
+	private String INTERFACE_URL;
+	private String COMPANY_NAME;
+	private String BODY_TYPE;
+	private String IS_DATA_SIGN;
+	private String SIGN_ALGORITHM;
+	private String SIGN_BODY;
+	private String CONTAIN_TYPE;
+	private String CONTAIN_RULE;
+	private String THIRD_PLATFORM;
 
 }
