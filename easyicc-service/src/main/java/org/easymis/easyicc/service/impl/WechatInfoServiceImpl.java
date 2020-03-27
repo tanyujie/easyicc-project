12d3fac6d8b6a7435e17b1ca3c1e35f18345930c
tanyujie <13551259347@139.com>
2020-03-27 11:43:34 +0800

    init

---
+++ easyicc-service/src/main/java/org/easymis/easyicc/service/impl/WechatInfoServiceImpl.java
@@ -0,0 +1,8 @@
+package org.easymis.easyicc.service.impl;
+
+import org.easymis.easyicc.service.WechatInfoService;
+import org.springframework.stereotype.Service;
+@Service
+public class WechatInfoServiceImpl implements WechatInfoService {
+
+}
