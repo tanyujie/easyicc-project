ed91f0a3a2def25a4fe149f4a2690c98d58a59b6
tanyujie <13551259347@139.com>
2020-03-27 10:26:10 +0800

    init

--- easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/controller/DistributionCardController.java
+++ easyicc-web/easyicc-card-distribution/src/main/java/org/easymis/easyicc/web/card/distribution/controller/DistributionCardController.java
@@ -6,18 +6,20 @@ import org.easymis.easyicc.common.result.RestResult;
 import org.easymis.easyicc.domain.entity.Card;
 import org.easymis.easyicc.web.card.distribution.service.DistributionCardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 
 @Api(value = "/distributionCard", description = "名片分配")
-@Controller
-@RequestMapping("/distributionCard")
+@RestController
+@RequestMapping("/distribution")
 public class DistributionCardController {
 	
 	@Autowired
 	private DistributionCardService service;
+	@RequestMapping(value = { "/allocationCard" }, method = { RequestMethod.GET, RequestMethod.POST })
 	RestResult allocationCard(List<Card> cardList, String serveName) {
 		return service.allocationCard(cardList, serveName);
 	}
