320e015037b4b9248e22280e9456d3a2d89ccb79
Administrator <Administrator@SC-201907201119>
2020-03-29 12:33:55 +0800

    init

---
+++ easyicc-web/easyicc-robot-server/src/main/java/org/easymis/easyicc/web/robot/controller/RobotController.java
@@ -0,0 +1,81 @@
+package org.easymis.easyicc.web.robot.controller;
+
+
+
+
+
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.Robot;
+import org.easymis.easyicc.service.RobotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+public class RobotController extends IdentityRepository{
+	@Autowired
+	private RobotService service;
+	
+	@ApiOperation(value = "查询接口", response = Robot.class)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),})
+	@RequestMapping(value = { "/findPage.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult findByOrgId(String name,Integer pageNum, Integer pageSize) {
+		String orgId = getOrgId();
+		Robot bean = new Robot();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		return RestResult.buildSuccess(service.find(bean, pageNum, pageSize));
+	}
+
+	@ApiOperation(value = "保存组织全局配置")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult add(Robot bean) {
+		if (service.save(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "修改组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult update(Robot bean) {
+		if (service.update(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "查看组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id", dataType = "string", required = false), })
+	@RequestMapping(value = { "/read.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult read(String id) {
+		return RestResult.buildSuccess(service.findById(id));
+	}
+
+	@ApiOperation(value = "删除组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id列表", dataType = "string", required = false), })
+	@RequestMapping(value = { "/delete.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult delete(String ids) {
+		return service.deleteByIds(ids);
+	}
+}
