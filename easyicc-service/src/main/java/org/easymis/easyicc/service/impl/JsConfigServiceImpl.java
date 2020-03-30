96c2ac3fa6aed370c36bed5c98bda05eccd1bf0e
tanyujie <13551259347@139.com>
2020-03-30 13:38:22 +0800

    init

--- easyicc-service/src/main/java/org/easymis/easyicc/service/impl/JsConfigServiceImpl.java
+++ easyicc-service/src/main/java/org/easymis/easyicc/service/impl/JsConfigServiceImpl.java
@@ -1,12 +1,17 @@
 package org.easymis.easyicc.service.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.easymis.easyicc.common.result.RestResult;
 import org.easymis.easyicc.domain.entity.JsConfig;
+import org.easymis.easyicc.domain.entity.Robot;
 import org.easymis.easyicc.mybatis.mapper.JsConfigMapper;
 import org.easymis.easyicc.service.JsConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 @Service
@@ -16,31 +21,34 @@ public class JsConfigServiceImpl implements JsConfigService{
 	@Override
 	public boolean save(JsConfig bean) {
 		// TODO Auto-generated method stub
-		return false;
+		return mapper.save(bean);
 	}
 
 	@Override
 	public boolean update(JsConfig bean) {
 		// TODO Auto-generated method stub
-		return false;
+		return mapper.update(bean);
 	}
 
 	@Override
 	public JsConfig findById(String id) {
 		// TODO Auto-generated method stub
-		return null;
+		return mapper.findById(id);
 	}
 
 	@Override
 	public PageInfo<?> find(JsConfig bean, Integer pageNum, Integer pageSize) {
-		// TODO Auto-generated method stub
-		return null;
+		PageHelper.startPage(pageNum, pageSize);
+		List<JsConfig> dataList = mapper.getList(bean);
+		PageInfo<JsConfig> p = new PageInfo<JsConfig>(dataList);
+		return p;
 	}
 
 	@Override
 	public RestResult deleteByIds(String ids) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> idsList = Arrays.asList(ids.split(","));
+		mapper.deleteBatch(idsList);
+		return RestResult.buildSuccess();
 	}
 
 }
