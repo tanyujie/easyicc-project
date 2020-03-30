7db606ad1daa9a14a2b747b8093002b1a5b43876
tanyujie <13551259347@139.com>
2020-03-30 19:11:17 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/DiyFormController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/DiyFormController.java
@@ -1,13 +1,81 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.DiyForm;
+import org.easymis.easyicc.service.DiyFormService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @Api(value = "/rewriteRule", description = "注册表单管理")
 @Controller
 @RequestMapping("/diyForm")
-public class DiyFormController {
+public class DiyFormController  extends IdentityRepository {
+	@Autowired
+	private DiyFormService service;
+	@ApiOperation(value = "查询接口", response = DiyForm.class)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),})
+	@RequestMapping(value = { "/findPage.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult findByOrgId(String name,Integer pageNum, Integer pageSize) {
+		String orgId = getOrgId();
+		DiyForm bean = new DiyForm();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		return RestResult.buildSuccess(service.find(bean, pageNum, pageSize));
+	}
 
+	@ApiOperation(value = "保存组织全局配置")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult add(DiyForm bean) {
+		if (service.save(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "修改组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult update(DiyForm bean) {
+		if (service.update(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "查看组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id", dataType = "string", required = false), })
+	@RequestMapping(value = { "/read.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult read(String id) {
+		return RestResult.buildSuccess(service.findById(id));
+	}
+
+	@ApiOperation(value = "删除组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id列表", dataType = "string", required = false), })
+	@RequestMapping(value = { "/delete.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult delete(String ids) {
+		return service.deleteByIds(ids);
+	}
 }
--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/OrganizeController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/OrganizeController.java
@@ -1,13 +1,81 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
+import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.Organize;
+import org.easymis.easyicc.service.OrganizeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @Api(value = "/organize", description = "公司信息配置")
 @Controller
 @RequestMapping("/organize")
-public class OrganizeController {
+public class OrganizeController extends IdentityRepository{
+	@Autowired
+	private OrganizeService service;
+	@ApiOperation(value = "查询接口", response = Organize.class)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),})
+	@RequestMapping(value = { "/findPage.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult findByOrgId(String name,Integer pageNum, Integer pageSize) {
+		String orgId = getOrgId();
+		Organize bean = new Organize();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		return RestResult.buildSuccess(service.find(bean, pageNum, pageSize));
+	}
 
+	@ApiOperation(value = "保存组织全局配置")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult add(Organize bean) {
+		if (service.save(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "修改组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
+		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
+	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult update(Organize bean) {
+		if (service.update(bean))
+			return RestResult.buildSuccess();
+		else
+			return RestResult.buildFail();
+	}
+
+	@ApiOperation(value = "查看组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id", dataType = "string", required = false), })
+	@RequestMapping(value = { "/read.json" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult read(String id) {
+		return RestResult.buildSuccess(service.findById(id));
+	}
+
+	@ApiOperation(value = "删除组织全局配置信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "组织全局配置id列表", dataType = "string", required = false), })
+	@RequestMapping(value = { "/delete.do" }, method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public RestResult delete(String ids) {
+		return service.deleteByIds(ids);
+	}
 }
