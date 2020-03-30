380a8c503b155559c8b5796f8e91db599987228e
tanyujie <13551259347@139.com>
2020-03-30 17:59:07 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/HrmStaffInfoController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/HrmStaffInfoController.java
@@ -1,14 +1,13 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
 import org.easymis.easyicc.common.result.RestResult;
+import org.easymis.easyicc.domain.entity.HrmStaffInfo;
 import org.easymis.easyicc.domain.entity.HtmlAlias;
-import org.easymis.easyicc.domain.entity.JsConfig;
 import org.easymis.easyicc.domain.entity.PromotionChannel;
-import org.easymis.easyicc.service.HtmlAliasService;
+import org.easymis.easyicc.service.HrmStaffInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,24 +17,24 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
-@Api(value = "/htmlAlias", description = "网页别名设置")
+@Api(value = "/hrmStaffInfo", description = "客服账号管理")
 @Controller
-@RequestMapping("/htmlAlias")
-public class HtmlAliasController extends IdentityRepository{
+@RequestMapping("/hrmStaffInfo")
+public class HrmStaffInfoController extends IdentityRepository{
 	@Autowired
-	private HtmlAliasService service;
-	@ApiOperation(value = "网页别名首页")
+	private HrmStaffInfoService service;
+	@ApiOperation(value = "客服账号管理首页")
 	@RequestMapping(value = { "/index.html" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public String find(String name,Integer pageNum, Integer pageSize,ModelMap model) {
 		String orgId = getOrgId();
-		HtmlAlias bean = new HtmlAlias();
+		 HrmStaffInfo bean = new  HrmStaffInfo();
 		bean.setOrgId(orgId);
 		if (pageNum == null)
 			pageNum = 1;
 		if (pageSize == null)
 			pageSize = 10;
 		model.put("pageInfo", service.find(bean, pageNum, pageSize));
-		return "/htmlAlias/index";
+		return "/hrmStaff/index";
 	}
 	@ApiOperation(value = "查询接口", response = PromotionChannel.class)
 	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),})
@@ -43,7 +42,7 @@ public class HtmlAliasController extends IdentityRepository{
 	@ResponseBody
 	public RestResult findByOrgId(String name,Integer pageNum, Integer pageSize) {
 		String orgId = getOrgId();
-		HtmlAlias bean = new HtmlAlias();
+		HrmStaffInfo bean = new HrmStaffInfo();
 		bean.setOrgId(orgId);
 		if (pageNum == null)
 			pageNum = 1;
@@ -54,46 +53,46 @@ public class HtmlAliasController extends IdentityRepository{
 	@ApiOperation(value = "新增网页样式配置")
 	@RequestMapping(value = { "/add.html" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public String add() {
-		return "/htmlAlias/add";
+		return "/HrmStaffInfo/add";
 	}
-	@ApiOperation(value = "保存网页别名")
+	@ApiOperation(value = "保存客服账号管理")
 	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
 		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
 		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
 		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
 	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public RestResult save(HtmlAlias bean) {
+	public RestResult save(HrmStaffInfo bean) {
 		if (service.save(bean))
 			return RestResult.buildSuccess();
 		else
 			return RestResult.buildFail();
 	}
 
-	@ApiOperation(value = "修改网页别名信息")
+	@ApiOperation(value = "修改客服账号管理信息")
 	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),
 		@ApiImplicitParam(name = "priority", value = "排序", dataType = "int", required = false),
 		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
 		@ApiImplicitParam(name = "depict", value = "备注", dataType = "string", required = false),})
 	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public RestResult update(HtmlAlias bean) {
+	public RestResult update(HrmStaffInfo bean) {
 		if (service.update(bean))
 			return RestResult.buildSuccess();
 		else
 			return RestResult.buildFail();
 	}
 
-	@ApiOperation(value = "查看网页别名信息")
-	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "网页别名id", dataType = "string", required = false), })
+	@ApiOperation(value = "查看客服账号管理信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "客服账号管理id", dataType = "string", required = false), })
 	@RequestMapping(value = { "/read.json" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
 	public RestResult read(String id) {
 		return RestResult.buildSuccess(service.findById(id));
 	}
 
-	@ApiOperation(value = "删除网页别名信息")
-	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "网页别名id列表", dataType = "string", required = false), })
+	@ApiOperation(value = "删除客服账号管理信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "ids", value = "客服账号管理id列表", dataType = "string", required = false), })
 	@RequestMapping(value = { "/delete.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
 	public RestResult delete(String ids) {
