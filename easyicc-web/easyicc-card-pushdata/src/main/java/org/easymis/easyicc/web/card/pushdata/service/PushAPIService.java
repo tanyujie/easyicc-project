744911fecaeadb724dbe9d44cee63472570d2e49
tanyujie <13551259347@139.com>
2020-03-30 11:41:37 +0800

    init

--- easyicc-web/easyicc-card-pushdata/src/main/java/org/easymis/easyicc/web/card/pushdata/service/PushAPIService.java
+++ easyicc-web/easyicc-card-pushdata/src/main/java/org/easymis/easyicc/web/card/pushdata/service/PushAPIService.java
@@ -1,6 +1,8 @@
 package org.easymis.easyicc.web.card.pushdata.service;
 
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.easymis.easyicc.domain.entity.CardPushConfig;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RocketMQMessageListener(topic = "api-topic", consumerGroup = "api-group")
-public class PushAPIService {
+public class PushAPIService implements RocketMQListener<CardPushConfig> {
+	@Override
+	public void onMessage(CardPushConfig message) {
+		// TODO Auto-generated method stub
 
+	}
 }
--- easyicc-web/easyicc-card-pushdata/src/main/java/org/easymis/easyicc/web/card/pushdata/service/PushWechatService.java
+++ easyicc-web/easyicc-card-pushdata/src/main/java/org/easymis/easyicc/web/card/pushdata/service/PushWechatService.java
@@ -1,6 +1,8 @@
 package org.easymis.easyicc.web.card.pushdata.service;
 
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.easymis.easyicc.domain.entity.WechatInfo;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RocketMQMessageListener(topic = "wechat-topic", consumerGroup = "wechat-group")
-public class PushWechatService {
+public class PushWechatService implements RocketMQListener<WechatInfo> {
+	@Override
+	public void onMessage(WechatInfo wechatInfo) {
+		// TODO Auto-generated method stub
+		log.info("received message: " + wechatInfo);
+	}
 
 }
