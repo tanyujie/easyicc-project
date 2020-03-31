588ee4726cdcb27e676911837113a73b562bcad1
Administrator <Administrator@SC-201907201119>
2020-03-31 22:40:03 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/ClientApiStarterApplication.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/ClientApiStarterApplication.java
@@ -10,14 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**Jishubu1   easyliao2020**/
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@EnableDiscoveryClient
+/*@EnableDiscoveryClient*/
 @ComponentScan({"*.easymis.easyicc.*"})
 @EnableTransactionManagement(order = 2) // 设置事务执行顺序(需要在切换数据源之后，否则只走主库)
 @MapperScan({"*.easymis.easyicc.**.mapper"})
--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/SiteController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/SiteController.java
@@ -1,10 +1,13 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
+import javax.validation.Valid;
+
 import org.easymis.easyicc.common.result.RestResult;
 import org.easymis.easyicc.domain.entity.Site;
 import org.easymis.easyicc.service.SiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +24,20 @@ import io.swagger.annotations.ApiOperation;
 public class SiteController extends IdentityRepository{
 	@Autowired
 	private SiteService service;
-	@ApiOperation(value = "��ѯ�ӿ�", response = Site.class)
+	@ApiOperation(value = "站点管理首页")
+	@RequestMapping(value = { "/index.html" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String find(String name,Integer pageNum, Integer pageSize,ModelMap model) {
+		String orgId = getOrgId();
+		Site bean = new Site();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		model.put("pageInfo", service.find(bean, pageNum, pageSize));
+		return "/site/index";
+	}
+	@ApiOperation(value = "站点查询", response = Site.class)
 	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "��������", dataType = "string", required = false),})
 	@RequestMapping(value = { "/findPage.json" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
@@ -35,29 +51,32 @@ public class SiteController extends IdentityRepository{
 			pageSize = 10;
 		return RestResult.buildSuccess(service.find(bean, pageNum, pageSize));
 	}
-
-	@ApiOperation(value = "������֯ȫ������")
-	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "��������", dataType = "string", required = false),
-		@ApiImplicitParam(name = "priority", value = "����", dataType = "int", required = false),
-		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
-		@ApiImplicitParam(name = "depict", value = "��ע", dataType = "string", required = false),})
+	@ApiOperation(value = "新增子站点")
+	@RequestMapping(value = { "/add.html" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String add(ModelMap model) {
+		String orgId = getOrgId();
+		
+		return "/site/add";
+	}
+	@ApiOperation(value = "保存子站点")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "子站点名称", dataType = "string", required = false),
+			@ApiImplicitParam(name = "depict", value = "子站点描述", dataType = "string", required = false), })
 	@RequestMapping(value = { "/save.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public RestResult add(Site bean) {
+	public RestResult save(@Valid Site bean) {
+		bean.setOrgId(getOrgId());
 		if (service.save(bean))
 			return RestResult.buildSuccess();
 		else
 			return RestResult.buildFail();
 	}
 
-	@ApiOperation(value = "�޸���֯ȫ��������Ϣ")
-	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "��������", dataType = "string", required = false),
-		@ApiImplicitParam(name = "priority", value = "����", dataType = "int", required = false),
-		@ApiImplicitParam(name = "parentId", value = "parentId", dataType = "string", required = false),
-		@ApiImplicitParam(name = "depict", value = "��ע", dataType = "string", required = false),})
+	@ApiOperation(value = "修改子站点信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "子站点名称", dataType = "string", required = false),
+		@ApiImplicitParam(name = "depict", value = "子站点描述", dataType = "string", required = false), })
 	@RequestMapping(value = { "/update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public RestResult update(Site bean) {
+	public RestResult update(@Valid Site bean) {
 		if (service.update(bean))
 			return RestResult.buildSuccess();
 		else
