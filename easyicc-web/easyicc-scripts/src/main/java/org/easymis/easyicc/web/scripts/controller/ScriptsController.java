b1d65d8d89a3aba54494ced640868ffe470602a6
Administrator <Administrator@SC-201907201119>
2020-04-01 20:28:11 +0800

    initial

--- easyicc-web/easyicc-scripts/src/main/java/org/easymis/easyicc/web/scripts/controller/ScriptsController.java
+++ easyicc-web/easyicc-scripts/src/main/java/org/easymis/easyicc/web/scripts/controller/ScriptsController.java
@@ -1,11 +1,14 @@
 package org.easymis.easyicc.web.scripts.controller;
 
+import org.easymis.easyicc.domain.entity.JsConfig;
 import org.easymis.easyicc.domain.vo.answer.AnswerVo;
-import org.easymis.easyicc.domain.vo.question.QuestionVo;
+import org.easymis.easyicc.service.JsConfigService;
 import org.easymis.easyicc.service.RobotChatDetailService;
 import org.easymis.easyicc.service.RobotQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,17 +21,19 @@ import io.swagger.annotations.ApiOperation;
 @Controller
 @RequestMapping("/")
 public class ScriptsController extends IdentityRepository{
-
 	@Autowired
-	private RobotQuestionService service;
+	private JsConfigService jsConfigService;
+	@Autowired
+	private RobotQuestionService robotQuestionService;
 	@Autowired
 	private RobotChatDetailService chatDetailService;
 	
 	@ApiOperation(value = "查询接口", response = AnswerVo.class)
 	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "分类名称", dataType = "string", required = false),})
-	@RequestMapping(value = { "/{orgId}/{pageId}.js" }, method = { RequestMethod.GET, RequestMethod.POST })
-	public String index(QuestionVo questionVo) {
-
+	@RequestMapping(value = { "/{pageId}.js" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String index(@PathVariable String pageId,ModelMap model) {
+		JsConfig jsConfig=jsConfigService.findById(pageId);
+		model.put("jsConfig", jsConfig);
 		return "/index";
 	}
 }
