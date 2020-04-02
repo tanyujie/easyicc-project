e60647a5c2d45f12fcf46929c490bc1fa8b1f124
Administrator <Administrator@10.0.0.1>
2020-04-02 22:37:00 +0800

    init

--- easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/MsgController.java
+++ easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/MsgController.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MsgController {
 
-	@RequestMapping("/msg")
-	public String index(String cmd) throws NoSuchAlgorithmException {
+	@RequestMapping("/msg.do")
+	public String index(String cmd, String v, String u, String userId, String c, String ext, String keys[],
+			String values[], String promotionId, String tag, String ref, Integer ocpcPlatform, Integer ocpcCondition,
+			String ocpcConfigId, String g, String chatType, String sid) throws NoSuchAlgorithmException {
 		if (cmd.equals("getMessage"))
 			return getMessage("orgId");
 		else if (cmd.equals("addEvent"))
 			return addEvent("orgId");
 		else if (cmd.equals("chat"))
-			return chat("orgId");
+			return chat( v,  u,  userId,  c,  ext,  keys,
+					 values,  promotionId,  tag,  ref,  ocpcPlatform,  ocpcCondition,
+					 ocpcConfigId,  g,  chatType,  sid);
 
 		return "/customerService";
 	}
@@ -30,7 +34,9 @@ public class MsgController {
 		return "/msg/addEvent";
 	}
 
-	private String chat(String orgId) throws NoSuchAlgorithmException {
+	private String chat(String v, String u, String userId, String c, String ext, String keys[],
+			String values[], String promotionId, String tag, String ref, Integer ocpcPlatform, Integer ocpcCondition,
+			String ocpcConfigId, String g, String chatType, String sid) throws NoSuchAlgorithmException {
 
 		return "/msg/chat";
 	}
