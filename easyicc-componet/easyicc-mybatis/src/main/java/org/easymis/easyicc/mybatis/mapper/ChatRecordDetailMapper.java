815d194f022abe8c1b3b575709f37b4e17eb2a46
Administrator <Administrator@SC-201907201119>
2020-04-03 23:10:51 +0800

    init

--- easyicc-componet/easyicc-mybatis/src/main/java/org/easymis/easyicc/mybatis/mapper/ChatRecordDetailMapper.java
+++ easyicc-componet/easyicc-mybatis/src/main/java/org/easymis/easyicc/mybatis/mapper/ChatRecordDetailMapper.java
@@ -1,5 +1,25 @@
 package org.easymis.easyicc.mybatis.mapper;
 
-public interface ChatRecordDetailMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.easymis.easyicc.domain.entity.ChatRecordDetail;
+import org.easymis.easyicc.domain.vo.ChatRecordDetailVo;
+
+public interface ChatRecordDetailMapper {
+	@Insert("insert into chat_record_detail(org_id, chat_id, recorder_id, create_Time, type, message, from_id)"
+			+ "values"
+			+ "(#{orgId},#{chatId},#{recorderId},#{createTime},#{type},#{message},#{fromId})")
+	boolean insertByBean(ChatRecordDetail bean);
+	@Select("<script>" + "SELECT * FROM chat_record_detail "+
+            " <where> " +
+            " <if test=\"orgId != null\">org_id=#{orgId}</if> " +
+            " <if test=\"chatId != null\"> AND chat_id=#{chatId}</if> " +
+            " </where> " +
+            "<if test='start!=null'>" +
+            " LIMIT #{start}" +
+            "</if>" +
+			"</script>")
+	public List<ChatRecordDetail> findList(ChatRecordDetailVo bean);
 }
