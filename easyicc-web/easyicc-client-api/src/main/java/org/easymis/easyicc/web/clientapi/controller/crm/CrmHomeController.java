e1735e31a6dd17fa94cd8ebf417fc07e3ddd27d3
Administrator <Administrator@SC-201907201119>
2020-04-04 20:46:28 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/crm/CrmHomeController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/crm/CrmHomeController.java
@@ -1,14 +1,34 @@
 package org.easymis.easyicc.web.clientapi.controller.crm;
 
+import org.easymis.easyicc.domain.entity.ChatRecord;
+import org.easymis.easyicc.service.ChatRecordService;
 import org.easymis.easyicc.web.clientapi.controller.IdentityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Api(value = "/cardsMan", description = "imCRM首页")
 @Controller
 @RequestMapping("/im/crm/home")
 public class CrmHomeController extends IdentityRepository{
-
+	@Autowired
+	private ChatRecordService service;
+	@ApiOperation(value = "imCRM首页")
+	@RequestMapping(value = { "/index.html" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String find(String name,Integer pageNum, Integer pageSize,ModelMap model) {
+		String orgId = getOrgId();
+		ChatRecord bean = new ChatRecord();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		model.put("pageInfo", service.find(bean, pageNum, pageSize));
+		return "/im/card/cardsMan";
+	}
 }
--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/crm/VisitorAnalyseController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/crm/VisitorAnalyseController.java
@@ -1,13 +1,36 @@
 package org.easymis.easyicc.web.clientapi.controller.crm;
 
+import org.easymis.easyicc.domain.entity.ChatRecord;
+import org.easymis.easyicc.service.ChatRecordService;
+import org.easymis.easyicc.service.VisitorInfoService;
 import org.easymis.easyicc.web.clientapi.controller.IdentityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.swagger.annotations.Api;
-@Api(value = "/leaveMessageMan", description = "im客户端营销诊断")
+import io.swagger.annotations.ApiOperation;
+@Api(value = "/visitor/analyse", description = "im客户端营销诊断")
 @Controller
-@RequestMapping("/visitor/analyse/index")
+@RequestMapping("/visitor/analyse")
 public class VisitorAnalyseController extends IdentityRepository {
-
+	@Autowired
+	private ChatRecordService service;
+	@Autowired
+	private VisitorInfoService visitorInfoService;
+	@ApiOperation(value = "im客户端营销诊断")
+	@RequestMapping(value = { "/index.html" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String find(String name,Integer pageNum, Integer pageSize,ModelMap model) {
+		String orgId = getOrgId();
+		ChatRecord bean = new ChatRecord();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		model.put("pageInfo", service.find(bean, pageNum, pageSize));
+		return "/im/visitor/analyse/index";
+	}
 }
