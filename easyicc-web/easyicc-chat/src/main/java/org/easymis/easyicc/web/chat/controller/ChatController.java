5803b99814b59ae91172d5c8df2c792c94dc36f8
Administrator <Administrator@SC-201907201119>
2020-04-06 23:34:43 +0800

    init

--- easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/ChatController.java
+++ easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/ChatController.java
@@ -2,21 +2,27 @@ package org.easymis.easyicc.web.chat.controller;
 
 import java.security.NoSuchAlgorithmException;
 
+import org.easymis.easyicc.domain.entity.ChatRecord;
+import org.easymis.easyicc.service.ChatRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ChatController {
-
+	@Autowired
+    private ChatRecordService chatRecordService;
 	@RequestMapping("/chat.do")
-	public String index(String o,String v,String u,String config,String chatUrl) throws NoSuchAlgorithmException {
-		ModelAndView mav = new ModelAndView();  
-		mav.addObject("orgId", "o");
-		mav.addObject("visitorId", "v");
-		mav.addObject("staffId", "u");
-		mav.addObject("jsConfigId", "config");
-		mav.addObject("chatUrl", "chatUrl");
+	public String index(String o,String v,String u,String config,String chatUrl,Model model) throws NoSuchAlgorithmException {
+		model.addAttribute("orgId", o);
+		model.addAttribute("visitorId", v);
+		model.addAttribute("staffId", u);
+		model.addAttribute("jsConfigId", config);
+		model.addAttribute("chatUrl", chatUrl);
+		ChatRecord chatRecord=chatRecordService.findByVisitorId(v);
+		model.addAttribute("chatId", chatRecord.getChatId());
 		return "/chat";
 	}
 	@RequestMapping("/blank.html")
--- easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/MsgController.java
+++ easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/controller/MsgController.java
@@ -1,6 +1,7 @@
 package org.easymis.easyicc.web.chat.controller;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 import org.easymis.easyicc.domain.entity.ChatRecord;
 import org.easymis.easyicc.domain.entity.ChatRecordDetail;
@@ -12,6 +13,7 @@ import org.easymis.easyicc.service.VisitorInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MsgController {
@@ -22,7 +24,8 @@ public class MsgController {
 	@Autowired
 	private VisitorInfoService visitorInfoService;
 	@RequestMapping("/msg.do")
-	public String index(String cmd, String v, String u, String userId, String c, String ext, String keys[],
+	@ResponseBody
+	public HashMap index(String cmd, String v, String u, String userId, String c, String ext, String keys[],
 			String values[], String promotionId, String tag, String ref, Integer ocpcPlatform, Integer ocpcCondition,
 			String ocpcConfigId, String g, String chatType, String sid,String cId,Integer start,Integer vStart,String msg,Integer force) throws NoSuchAlgorithmException {
 		if (cmd.equals("getMessage"))
@@ -35,8 +38,8 @@ public class MsgController {
 			return chat( v,  u,  userId,  c,  ext,  keys,
 					 values,  promotionId,  tag,  ref,  ocpcPlatform,  ocpcCondition,
 					 ocpcConfigId,  g,  chatType,  sid,cId);
-
-		return "/customerService";
+		return null;
+		//return "/customerService";
 	}
 	/**
 	 * 
@@ -49,14 +52,16 @@ public class MsgController {
 	　 * @param force
 	　 * @return
 	 */
-	private String addMessage(String orgId,String chatId,String fromId,String message,Integer force) {
+	private HashMap addMessage(String orgId,String chatId,String fromId,String message,Integer force) {
 		ChatRecordDetail bean= new ChatRecordDetail();
 		bean.setOrgId(orgId);
 		bean.setChatId(chatId);
 		bean.setMessage(message);
-		bean.setFromId(fromId);
+		bean.setFromUserId(fromId);
+		bean.setToUserId("AI-ylkj");
 		chatRecordDetailService.save(bean);
-		return "/msg/addMessage";
+		return new HashMap();
+		//return "/msg/addMessage";
 	}
 /** 
 *<p>Title: getMessage</p>
@@ -70,20 +75,34 @@ public class MsgController {
 　 * @return
 　 * @throws NoSuchAlgorithmException
  */
-	private String getMessage(String orgId,String visitorId,String u,String chatId,Integer start,Integer vStart) throws NoSuchAlgorithmException {
+	
+	private HashMap getMessage(String orgId,String visitorId,String u,String chatId,Integer start,Integer vStart) throws NoSuchAlgorithmException {
+		HashMap<String, Object> map = new HashMap<String, Object>();
 		ChatRecordDetailVo vo = new ChatRecordDetailVo();
-		chatRecordDetailService.findList(vo);
-		return "/msg/getMessage";
+		vo.setChatId(chatId);
+		if(start==null)
+			start=0;
+		if(vStart==null)
+			vStart=0;
+		if (start > vStart)
+			vo.setStart(start);
+		else
+			vo.setStart(vStart);
+		map.put("result", "success");
+		map.put("msgs", chatRecordDetailService.findList(vo));
+		return map;
+		//return "/msg/getMessage";
 	}
 
-	private String addEvent(String orgId) throws NoSuchAlgorithmException {
-
-		return "/msg/addEvent";
+	private HashMap addEvent(String orgId) throws NoSuchAlgorithmException {
+		return new HashMap();
+		//return "/msg/addEvent";
 	}
 
-	private String chat(String visitorId, String u, String userId, String orgId, String ext, String keys[],
+	private HashMap chat(String visitorId, String u, String userId, String orgId, String ext, String keys[],
 			String values[], String promotionId, String tag, String ref, Integer ocpcPlatform, Integer ocpcCondition,
 			String ocpcConfigId, String g, String chatType, String sid,String chatId) throws NoSuchAlgorithmException {
+		HashMap map = new HashMap();
 		ChatRecord bean= new ChatRecord();
 		bean.setOrgId(orgId);
 		bean.setChatId(chatId);
@@ -95,6 +114,34 @@ public class MsgController {
 		visitorInfo.setOrgId(orgId);
 		visitorInfo.setChatId(chatId);
 		//visitorInfoService.saveOrUpdate(visitorInfo);
-		return "/msg/chat";
+		map.put("result", "success");
+		map.put("isChatExists", true);
+		HashMap userInfo= new HashMap();
+		userInfo.put("timesOfToVisitorMsg", "2");
+		userInfo.put("replyMsgAtConnected", "我是今天的值班客服，请问您需要咨询哪方面的问题？为了更好的服务效果，请在对话结束后对我的服务满意度进行评价，非常感谢您的支持。");
+		userInfo.put("phoneNumber", null);
+		userInfo.put("replyMsgOfCustToVisitor", "已经很久没有收到您的消息了，请问您还在电脑前吗？如果没有其他的问题我将主动关闭该对话。随时欢迎您再次向我咨询，祝您今天好心情。");
+		userInfo.put("chatAutoCloseMsg", "非常感谢您的光临，因长时间未对话，系统将自动关闭该对话！");
+		userInfo.put("nickName", "在线客服");
+		userInfo.put("customerCardImg", "");
+		userInfo.put("cycleTimeOfToVisitorMsg", "60");
+		userInfo.put("department", "客服部");
+		userInfo.put("userId", "AI-ylkj");
+		userInfo.put("chatAutoCloseTime", "30");
+		userInfo.put("email", null);
+		map.put("userInfo", userInfo);
+		
+		map.put("chatId", chatId);
+		map.put("waitIndex", null);
+		map.put("groupId", "24135");
+		map.put("type", "CHATING");
+		map.put("waitSendMessage", "");
+		map.put("visitorId", visitorId);
+		
+		
+		
+		
+		return map;
+		//return "/msg/chat";
 	}
 }
