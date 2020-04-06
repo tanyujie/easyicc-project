e1fa62b95271100b715d41d4c6188b5a6f7ddde3
Administrator <Administrator@SC-201907201119>
2020-04-06 23:33:39 +0800

    init

--- easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/ChatRecordDetail.java
+++ easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/ChatRecordDetail.java
@@ -8,12 +8,21 @@ import lombok.Data;
 @Data
 public class ChatRecordDetail implements Serializable {
 
-	private String recorderId;//RECORDER_ID
-	private Date createTime; 
-	private String chatId;
-	private String message;
-	private String fromId;
-	private Integer type;
-	private String orgId;
+	  //组织id",column="org_id",length=40,isnull=false) 
+      private String orgId; 
+      //对话id",column="chat_id",length=40,isnull=false) 
+      private String chatId; 
+      //客服id",column="recorder_id",length=40,isnull=true) 
+      private String recorderId; 
+      //发送时间",column="create_time",isnull=false) 
+      private Date createTime; 
+      //",column="type",isnull=true) 
+      private String type; 
+      //消息内容",column="message",length=255,isnull=true) 
+      private String message; 
+      //客服id",column="from_staff_id",length=40,isnull=true) 
+      private String fromUserId; 
+      //访客id",column="to_user_id",length=40,isnull=true) 
+      private String toUserId; 
 
 }
