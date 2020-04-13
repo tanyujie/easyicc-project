21489edd33ce40be605da11a3dd82d635f664568
Administrator <Administrator@SC-201907201119>
2020-04-13 19:19:30 +0800

    init

--- easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/ServiceConsoleIndexController.java
+++ easyicc-web/easyicc-client-api/src/main/java/org/easymis/easyicc/web/clientapi/controller/ServiceConsoleIndexController.java
@@ -1,13 +1,29 @@
 package org.easymis.easyicc.web.clientapi.controller;
 
+import org.easymis.easyicc.domain.entity.JsConfig;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-@Api(value = "/service/Console/Index", description = "设置中心")
+@Api(value = "/console/index", description = "设置中心")
 @Controller
-@RequestMapping("/service/Console/Index")
+@RequestMapping("/console")
 public class ServiceConsoleIndexController  extends IdentityRepository{
-
+	@ApiOperation(value = "设置中心首页")
+	@RequestMapping(value = { "/index.html" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public String find(String name,Integer pageNum, Integer pageSize,ModelMap model) {
+		String orgId = getOrgId();
+		JsConfig bean = new JsConfig();
+		bean.setOrgId(orgId);
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 10;
+		//model.put("pageInfo", service.find(bean, pageNum, pageSize));
+		return "/console/index";
+	}
 }
