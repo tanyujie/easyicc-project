698685b279851dea7e0f63bd7a0138d5a68c359f
Administrator <Administrator@SC-201907201119>
2020-04-19 16:30:48 +0800

    init

---
+++ easyicc-web/easyicc-crm/src/main/java/org/easymis/easyicc/web/crm/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package org.easymis.easyicc.web.crm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	 public void addInterceptors(InterceptorRegistry registry) {
+	        registry.addInterceptor(new BasePathInterceptor()).addPathPatterns("/**");
+	    }
+}
