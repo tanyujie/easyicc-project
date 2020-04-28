cd7855b15dd68152525383ece992cbc9c61729ae
Administrator <Administrator@SC-201907201119>
2020-04-28 16:45:16 +0800

    init

--- easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/RobotQuestionCategory.java
+++ easyicc-componet/easyicc-domain/src/main/java/org/easymis/easyicc/domain/entity/RobotQuestionCategory.java
@@ -12,6 +12,8 @@ public class RobotQuestionCategory {
     private String rebotId; 
     //语料库名称",column="category_name",length=255,isnull=true) 
     private String categoryName; 
+    //分类描述",column="depict",length=255,isnull=true) 
+    private String depict; 
     //创建时间",column="create_time",isnull=true) 
     private Date createTime; 
     //修改时间",column="update_time",isnull=true) 
