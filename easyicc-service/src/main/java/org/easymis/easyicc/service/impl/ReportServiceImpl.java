c0bb03e7e1bb5061da1795706da6fffaf9da7997
Administrator <Administrator@SC-201907201119>
2020-04-30 11:22:00 +0800

    init

---
+++ easyicc-service/src/main/java/org/easymis/easyicc/service/impl/ReportServiceImpl.java
@@ -0,0 +1,18 @@
+package org.easymis.easyicc.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.easymis.easyicc.domain.entity.CustomerReport;
+import org.easymis.easyicc.service.ReportService;
+import org.springframework.stereotype.Service;
+@Service
+public class ReportServiceImpl implements ReportService {
+
+	@Override
+	public List<CustomerReport> getCustomerReport(Map<String, Object> params, String startTime, String endTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
