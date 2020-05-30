b227d3797130b40db1ad773a4192972b23da51a4
Administrator <Administrator@10.0.0.1>
2020-05-30 09:48:34 +0800

    init

---
+++ easyicc-web/easyicc-chat/src/main/java/org/easymis/easyicc/web/chat/config/BasePathInterceptor.java
@@ -0,0 +1,26 @@
+package org.easymis.easyicc.web.chat.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class BasePathInterceptor extends HandlerInterceptorAdapter{
+	private static Logger logger = LoggerFactory.getLogger(BasePathInterceptor.class);
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	        String scheme = request.getScheme();
+	        String serverName = request.getServerName();
+	        int port = request.getServerPort();
+	        
+	        String path = request.getContextPath();
+	        String basePath = scheme + "://" + serverName + ":" + port + path;
+	        if(port==80)
+	        	basePath="http://www.easymis.cn";
+	        logger.info(basePath);
+	        request.setAttribute("basePath", basePath);
+	        return true;
+	    }
+}
